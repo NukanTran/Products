e405dc10a3335bad0f3fd1c10672b118db86f47b
TPSSOFT\lam.tran <tranhoanglamkcr@gmail.com>
2017-09-21 17:32:52 +0700

    new project

---
+++ Products/DAO/ProductDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Products.Models;
+
+namespace Products.DAO
+{
+    public class ProductDAO
+    {
+
+        protected readonly ProductsEntities2 context;
+
+        ProductDAO(ProductsEntities2 context)
+        {
+            this.context = context;
+        }
+        
+        public List<Product> GetList()
+        {
+            return context.Products.ToList();
+        }
+    }
+}
